8c883ae927f278c2413eef64405c8283cd2510ac
hepona <hepona10@gmail.com>
2024-04-25 00:51:13 +0100

    0 +1

--- 0x01-ES6_promise/0-promise.test.js
+++ 0x01-ES6_promise/0-promise.test.js
@@ -1,5 +1,11 @@
 import { getResponseFromAPI } from './0-promise.js';
 
 test('tasks are correctly defined', () => {
-  expect(`${getResponseFromAPI()}`).toEqual(true);
+  expect(getResponseFromAPI()).toEqual(true);
 });
+
+// import { taskFirst, taskNext } from './0-constants.js';
+
+// test('tasks are correctly defined', () => {
+//   expect(`${taskFirst()} ${taskNext()}`).toEqual("I prefer const when I can. But sometimes let is okay");
+// });
