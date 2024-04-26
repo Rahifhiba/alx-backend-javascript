f17b4f949953d97611eca9f275c5048a8517b6af
hepona <hepona10@gmail.com>
2024-04-27 00:59:10 +0100

    5+6

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,9 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
-    let comb = [array1, array2, string]
-
-    return  comb
+  return [...array1, ...array2, ...string];
 }
