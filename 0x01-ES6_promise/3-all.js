b63df0c0379ff86ef5b0e35b77e28e4fbbb5ebdd
hepona <hepona10@gmail.com>
2024-04-27 03:59:40 +0100

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from "./utils"
+
+export default function handleProfileSignup() {
+    return new Promise((resolve, reject) => {
+        let u = new createUser
+        if (createUser){
+            resolve(() => {u.firstname, u.lastname})
+        }
+        else {
+            reject(console.log("Signup system offline"))
+        }
+    })
+}
