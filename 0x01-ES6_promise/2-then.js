1ad44d35371c620db77df0503b6b3e46a214f32b
hepona <hepona10@gmail.com>
2024-04-27 03:33:58 +0100

    1+2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
+      console.log('Got a response from the API');
+    } else {
+      reject(new Error(''));
+      console.log('Got a response from the API');
+    }
+  });
+}
