5e1d54845622094d7cfff43c23a83fa09faf9dc6
hepona <hepona10@gmail.com>
2024-04-27 02:46:31 +0100

    12

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,8 @@
 export default function createIteratorObject(report) {
-    
+    let arr = []
+    for (let i = 0; i < report.length; i++) {
+        arr.push(report[i])
+
+    }
+    return arr
 }
