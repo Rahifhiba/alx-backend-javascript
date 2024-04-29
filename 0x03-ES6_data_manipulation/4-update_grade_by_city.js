92348d0dd398d61f18fc6a262dd5310338418a92
hepona <hepona10@gmail.com>
2024-04-30 00:59:38 +0100

    1+2+3

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  const specificStud = getListStudents.filter(
+    (getListStudents) => getListStudents.location === city,
+  );
+
+  const filtered = specificStud.map((specificStud) => {
+    const findGrade = newGrades.find((newGrades) => newGrades.studentId === specificStud.id);
+    const gradeCheck = findGrade ? findGrade.grade : 'N/A';
+    return { ...specificStud, grade: gradeCheck };
+  });
+  return filtered;
+}
