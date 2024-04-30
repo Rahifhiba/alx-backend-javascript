7f8c25de6f080413907b5d9bac741b7bf5c8cb7e
hepona <hepona10@gmail.com>
2024-05-01 00:00:00 +0100

    6+7

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function setFromArray(arr) {
+  const unique = new Set();
+  for (let i = 0; i < arr.length; i + 1) {
+    if (!unique.has(arr[i])) {
+      unique.add(arr[i]);
+    }
+  }
+  return unique;
+}
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function hasValuesFromArray(set, array) {
+  for (let i = 0; i < array.length; i + 1) {
+    if (!set.has(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
