d138118f117072e3312337da1f81c1a692596ffd
hepona <hepona10@gmail.com>
2024-04-30 23:35:51 +0100

    5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8view = new Int8Array(buffer);
+  if (position < 0 || position > int8view.length) {
+    throw new Error('Position outside range');
+  }
+  int8view[position] = value;
+  // const dataview = new DataView(buffer);
+  // dataview.getInt8(position)
+
+  return int8view;
+}
