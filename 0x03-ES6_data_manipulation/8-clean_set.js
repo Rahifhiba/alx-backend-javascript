b1d1fe53a09d322f31dc9dad95ef21189bc022cf
hepona <hepona10@gmail.com>
2024-05-01 01:59:47 +0100

    fix 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-    let final = ""
-    if (!startString.length == 0) {
-        for (const e of set) {
-            if (e.startsWith(startString)) {
-                final += e.slice(startString.length) + "-"
-            }
-        }
-        if (final.endsWith("-")) {
-            final = final.slice(0, -1);
-        }
+  let final = '';
+  if (startString.length !== 0) {
+    for (const e of set) {
+      if (e.startsWith(startString)) {
+        final += `${e.slice(startString.length) || ''}-`;
+      }
     }
-    return final
+    if (final.endsWith('-')) {
+      final = final.slice(0, -1);
+    }
+  }
+  return final;
 }
