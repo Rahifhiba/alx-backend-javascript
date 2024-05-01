564fac8472185aea9dae00f1de81b725c24c0499
hepona <hepona10@gmail.com>
2024-05-01 01:41:38 +0100

    8+10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+  map.forEach(
+    (v, k) => {
+      if (v === 1) map.set(k, 100);
+    },
+  );
+  return map;
+}
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-  let final = '';
-  if (!startString.length === 0) {
-    for (const e of set) {
-      if (e.startsWith(startString)) {
-        final += `${e.slice(startString.length)}-`;
-      }
+    let final = ""
+    if (!startString.length == 0) {
+        for (const e of set) {
+            if (e.startsWith(startString)) {
+                final += e.slice(startString.length) + "-"
+            }
+        }
+        if (final.endsWith("-")) {
+            final = final.slice(0, -1);
+        }
     }
-    if (final.endsWith('-')) {
-      final = final.slice(0, -1);
-    }
-  }
-  return final;
+    return final
 }
