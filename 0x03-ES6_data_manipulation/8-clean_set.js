46031f77afa5142f36dcae576542d4a8b5dfc879
hepona <hepona10@gmail.com>
2024-05-01 01:23:38 +0100

    8+9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  let final = '';
+  if (!startString.length === 0) {
+    for (const e of set) {
+      if (e.startsWith(startString)) {
+        final += `${e.slice(startString.length)}-`;
+      }
+    }
+    if (final.endsWith('-')) {
+      final = final.slice(0, -1);
+    }
+  }
+  return final;
+}
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function groceriesList() {
+  const map = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return map;
+}
