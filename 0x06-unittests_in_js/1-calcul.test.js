6c2077bb898fb9bfdce30c63eef56e393fb20da7
hepona <hepona10@gmail.com>
2024-11-28 18:34:21 +0100

    0+1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -12,11 +12,8 @@ describe("calculateNumber", function() {
   it("checks if -9-4=-13", function() {
     assert.equal(calculateNumber('SUBSTRACT',-9, 4), -13);
   });
-  it("checks if 9-4=-5", function() {
-    assert.equal(calculateNumber('SUBSTRACT',9, 4), 5);
-  });
-  it("checks if 9--4=13", function() {
-    assert.equal(calculateNumber('SUBSTRACT',9, -4), 13);
+  it("checks if 9.5-4=-5", function() {
+    assert.equal(calculateNumber('SUBSTRACT',9.5, 4), 6);
   });
   it("checks if 10/0='Error''", function() {
     assert.equal(calculateNumber('DIVIDE',10, 0), 'Error');
