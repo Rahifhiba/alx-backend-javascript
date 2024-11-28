c4cac1e3a05ab8ccc6a05ce1b306737cb90bf57d
hepona <hepona10@gmail.com>
2024-11-28 20:49:06 +0100

    0+1+2

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const Utils = require('./utils')
+function sendPaymentRequestToApi(totalAmount, totalShipping){
+    const tot = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    return `The total is: ${tot}`
+}
+
+module.exports = Utils
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const sinon =require('sinon')
+
+describe("calculateNumber", function() {
+    it("checks if 1.4+4.5=6", function() {
+        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      });
+      it("checks if 1.4-4.5=-4", function() {
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+      });
+      it("checks if 1.4/4.5=0.2", function() {
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+      });
+      it("checks if 1.4/0= error", function() {
+        expect( calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+      });
+    });
