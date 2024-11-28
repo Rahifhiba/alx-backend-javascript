cb697b0ca005ef54ef0a6152925388720d7d70bf
hepona <hepona10@gmail.com>
2024-11-28 20:16:21 +0100

    0+1+2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,23 +3,16 @@ const calculateNumber = require('./1-calcul');
 
 
 describe("calculateNumber", function() {
-  it("checks if 1+3=4", function() {
-    assert.equal(calculateNumber('SUM', 1.2, 3.23), 4);
+  it("checks if 1.4+4.5=6", function() {
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
   });
-  it("checks if 7.4/2=3.5", function() {
-    assert.equal(calculateNumber('DIVIDE', 7.4, 2.5), 2.3333333333333335);
+  it("checks if 1.4-4.5=-4", function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
-  it("checks if -9-4=-13", function() {
-    assert.equal(calculateNumber('SUBSTRACT',-9.5, 4.5), -14);
+  it("checks if 1.4/4.5=0.2", function() {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
-  it("checks if 9.5-4=-5", function() {
-    assert.equal(calculateNumber('SUBSTRACT',9.5, 4.5), 5);
+  it("checks if 1.4/0= error", function() {
+    assert.equal( calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
-  it("checks if 10/0='Error''", function() {
-    assert.equal(calculateNumber('DIVIDE',10, 0), 'Error');
-  });
-  it("checks if 0/0='Error''", function() {
-    assert.equal(calculateNumber('DIVIDE',0, 0), 'Error');
-  });
-
 });
