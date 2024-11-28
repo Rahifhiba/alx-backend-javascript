1d202a3c5629265b3d3aba107160ff9a3aa24abf
hepona <hepona10@gmail.com>
2024-11-28 19:22:24 +0100

    0+1+2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -18,6 +18,9 @@ describe("calculateNumber", function() {
   it("checks if 10/0='Error''", function() {
     assert.equal(calculateNumber('DIVIDE',10, 0), 'Error');
   });
+  it("checks if 0/0='Error''", function() {
+    assert.equal(calculateNumber('DIVIDE',0, 0), 'Error');
+  });
   it("checks if 5.7 - 2.3 = 4 (rounding up and down)", function() {
     assert.equal(calculateNumber('SUBSTRACT', 5.7, 2.3), 4);
   });
