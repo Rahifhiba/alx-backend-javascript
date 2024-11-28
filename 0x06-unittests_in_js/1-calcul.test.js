c6098f3d7320529bd98fcedf77b862b9865cee3e
hepona <hepona10@gmail.com>
2024-11-28 19:45:55 +0100

    0+1+2

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,29 +4,22 @@ const calculateNumber = require('./1-calcul');
 
 describe("calculateNumber", function() {
   it("checks if 1+3=4", function() {
-    assert.equal(calculateNumber('SUM', 1, 3), 4);
+    assert.equal(calculateNumber('SUM', 1.2, 3.23), 4);
   });
-  it("checks if 7/2=3.5", function() {
-    assert.equal(calculateNumber('DIVIDE', 7, 2), 3.5);
+  it("checks if 7.4/2=3.5", function() {
+    assert.equal(calculateNumber('DIVIDE', 7.4, 2.5), 2.3333333333333335);
   });
   it("checks if -9-4=-13", function() {
-    assert.equal(calculateNumber('SUBSTRACT',-9, 4), -13);
+    assert.equal(calculateNumber('SUBSTRACT',-9.5, 4.5), -14);
   });
   it("checks if 9.5-4=-5", function() {
-    assert.equal(calculateNumber('SUBSTRACT',9.5, 4), 6);
+    assert.equal(calculateNumber('SUBSTRACT',9.5, 4.5), 5);
   });
   it("checks if 10/0='Error''", function() {
     assert.equal(calculateNumber('DIVIDE',10, 0), 'Error');
   });
   it("checks if 0/0='Error''", function() {
     assert.equal(calculateNumber('DIVIDE',0, 0), 'Error');
-  });
-  it("checks if 5.7 - 2.3 = 4 (rounding up and down)", function() {
-    assert.equal(calculateNumber('SUBSTRACT', 5.7, 2.3), 4);
-  });
-
-  it("checks if -1.5 - -3.2 = 2 (negative numbers)", function() {
-    assert.equal(calculateNumber('SUBSTRACT', -1.5, -3.2), 2);
   });
 
 });
