3eb9bfb7eda14aa1d34fd8fd1c1fbf4f0e22c217
hepona <hepona10@gmail.com>
2024-11-29 09:46:51 +0100

    3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,11 +2,13 @@
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 const sinon =require('sinon')
+const expect = require('chai').expect
 
 describe("sendPaymentRequestToApi", function() {
-    it("checks ifutils match ", function() {
+    it("should call Utils.calculateNumber with the correct arguments", function() {
         const spy = sinon.spy(Utils, 'calculateNumber');
         sendPaymentRequestToApi(100, 20);
-        sinon.assert.calledWith(spy, 'SUM', 100, 20);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
         spy.restore();
     })})
