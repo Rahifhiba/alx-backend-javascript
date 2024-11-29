34b81c597611043e66214806f3a9700cf1d8414b
hepona <hepona10@gmail.com>
2024-11-29 14:18:08 +0100

    6+7

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const assert = require("assert");
+const getPaymentTokenFromAPI = require("./6-payment_token")
+describe("payement token", function () {
+
+    it("test if payement token return true", function(done) {
+        getPaymentTokenFromAPI(true)
+        .then((r) => {
+            assert.equal(r.data, 'Successful response from the API')
+            done();
+        })
+        .catch((error) => done(error));
+    })
+})
