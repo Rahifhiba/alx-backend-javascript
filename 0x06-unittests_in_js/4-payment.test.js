04bfc09c174f1c59ad810d0b8356cb4144c21439
hepona <hepona10@gmail.com>
2024-11-29 12:41:24 +0100

    3+4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 const sinon =require('sinon');
-const { expect } = require('chai');
 
 describe("sendPaymentRequestToApi", function() {
     it("should call Utils.calculateNumber with the correct arguments", function() {
         const func = sinon.stub(Utils, 'calculateNumber');
         func.returns(10)
+        const spy = sinon.spy(console, 'log');
         sendPaymentRequestToApi(100, 20)
-        sinon.assert.calledWith(func, 'SUM', 100, 20)
-        sinon.assert.calledWith(sinon.spy(console, 'log'), 'The total is: 10')
+        sinon.assert.calledWithExactly(func, 'SUM', 100, 20)
+        sinon.assert.calledWithExactly(spy, 'The total is: 10')
         func.restore();
+        spy.restore();
     });});
