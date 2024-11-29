87e3a8d7953bf75ebbde7609f09281b08f99d6b5
hepona <hepona10@gmail.com>
2024-11-29 13:45:58 +0100

    5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+const sinon =require('sinon');
+
+describe("sendPaymentRequestToApi", function() {
+    let spy;
+    beforeEach(function () {
+        spy = sinon.spy(console, 'log');
+      });
+    afterEach(function (){
+        spy.restore();
+    })
+    it("should call Utils.calculateNumber with 100 and 20", function() {
+        sendPaymentRequestToApi(100, 20)
+        sinon.assert.calledOnceWithExactly(spy, 'The total is: 120')
+    });
+    it("should call Utils.calculateNumber with 10 and 10", function() {
+        sendPaymentRequestToApi(10, 10)
+        sinon.assert.calledOnceWithExactly(spy, 'The total is: 20')
+    });
+});
