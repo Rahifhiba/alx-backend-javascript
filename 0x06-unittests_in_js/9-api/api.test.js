5bc6e050b017220bc1b6b85cf76ff58691775332
hepona <hepona10@gmail.com>
2024-11-30 02:00:26 +0100

    9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+const request = require('request')
+const assert = require('assert')
+
+describe("index", function() {
+    it("check if For the route GET /, return the message Welcome to the payment system", function() {
+        request('http://localhost:7865', function (err, resp, body) {
+            assert.equal(resp.statusCode, 200 );
+            assert.equal(body, 'Welcome to the payment system');
+        })
+
+    })
+    it("check if Correct status code when :id is a number", function() {
+        request('http://localhost:7865/cart/12', function (err, resp, body) {
+            assert.equal(resp.statusCode, 200);
+            assert.equal(body, 'Payment methods for cart 12')
+        })
+
+    })
+    it("check if Correct status code when :id is NOT a number", function() {
+        request('http://localhost:7865/cart/azdjoijzd', function (err, resp, body) {
+            assert.equal(resp.statusCode, 404);
+        })
+
+    })
+
+
+});
