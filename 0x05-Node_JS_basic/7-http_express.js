cddad18ea5f058213fa622331993f71c7c9d3c78
hepona <hepona10@gmail.com>
2024-11-30 02:50:06 +0100

    7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,42 +3,43 @@ const express = require('express');
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
-      const data = await fs.readFile(path, 'utf8');
-      const line = data.split('\n').filter((l) => l.trim() !== '');
-      const word = line.map((l) => l.split(','));
-      const NumStudent = word.length - 1;
-      let output = '';
-      output += `Number of students: ${NumStudent}\n`;
-      const ListStudCS = [];
-      const ListStudSWE = [];
-      let CountStudCS = 0;
-      let CountStudSWE = 0;
-      for (let i = 1; i <= NumStudent; i += 1) {
-        if (word[i].includes('CS')) {
-          ListStudCS.push(word[i][0]);
-          CountStudCS += 1;
-        }
-        if (word[i].includes('SWE')) {
-          ListStudSWE.push(word[i][0]);
-          CountStudSWE += 1;
-        }
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const line = data.split('\n').filter((l) => l.trim() !== '');
+    const word = line.map((l) => l.split(','));
+    const NumStudent = word.length - 1;
+    let output = '';
+    output += `Number of students: ${NumStudent}\n`;
+    const ListStudCS = [];
+    const ListStudSWE = [];
+    let CountStudCS = 0;
+    let CountStudSWE = 0;
+    for (let i = 1; i <= NumStudent; i += 1) {
+      if (word[i].includes('CS')) {
+        ListStudCS.push(word[i][0]);
+        CountStudCS += 1;
       }
-
-      output += `Number of students in CS: ${CountStudCS}. List: ${ListStudCS.join(', ')}\n`;
-      output += `Number of students in SWE: ${CountStudSWE}. List: ${ListStudSWE.join(', ')}`;
-      return output;
-    } catch (error) {
-      console.log('Cannot load the database');
-      return Promise.reject(error);
+      if (word[i].includes('SWE')) {
+        ListStudSWE.push(word[i][0]);
+        CountStudSWE += 1;
+      }
     }
+
+    output += `Number of students in CS: ${CountStudCS}. List: ${ListStudCS.join(', ')}\n`;
+    output += `Number of students in SWE: ${CountStudSWE}. List: ${ListStudSWE.join(', ')}`;
+    return output;
+  } catch (error) {
+    return console.log('Cannot load the database');
+    // return Promise.reject('Cannot load the database');
   }
+}
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
-app.get('/students', (req, res) => {
-    res.send("This is the list of our students\n", countStudents(process.argv[2]));
+app.get('/students', async (req, res) => {
+  const data = await countStudents(process.argv[2]);
+  res.send(`This is the list of our students\n${data}`);
 });
 app.listen(1245);
 
