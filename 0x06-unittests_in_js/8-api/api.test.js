9d5a0015e7ce6f7e375c9b9eb951c7d83922442e
hepona <hepona10@gmail.com>
2024-11-30 01:41:50 +0100

    8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request')
+const assert = require('assert')
+
+describe("index", function() {
+    it("check if For the route GET /, return the message Welcome to the payment system", function() {
+        request('http://localhost:7865', function (err, resp, body) {
+            assert.equal(resp.statusCode, 200 );
+            assert.equal(body, 'Welcome to the payment system');
+        })
+
+    })
+
+
+});
